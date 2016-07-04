a7bfbaf3b26fc30fbf664a98c8aca129f9c9ff3a
Nickolas A. Morrison <nickmorrison09@gmail.com>
2016-07-04 13:38:39 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$("document").ready(function(){
+    var navTopDelta = $("#about").offset().top+1;
+    $(window).scroll(function(){
+        var currentScroll = $(window).scrollTop();
+        if(currentScroll >= navTopDelta){
+            $("nav").css({
+                position: 'fixed',
+                top: '0',
+                left: '0',
+                boxShadow:'0 2px 2px gray'
+            });
+        }else{
+            $("nav").css({
+                position: 'absolute',
+                boxShadow:'none'
+            });
+        }
+    });
+});
