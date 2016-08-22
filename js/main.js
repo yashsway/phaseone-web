3ff46befcfad3bd7b306fe7b27b6b977aa46b721
Yash Gopal <reach.yashg@gmail.com>
2016-08-22 12:07:42 -0400

    Optimized SVG animation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+//Usually caps around twice the interval
 var allCircles = [];
-var count = 0;
+var circleCount = 0;
+var colorArray = ['#202426', '#BCD0DB', '#FFED93','#919DA2'];
+var colorCount = 0;
+colorArray = shuffle(colorArray);
 
 var s = Snap("#svg");
 
@@ -9,67 +13,110 @@ const grow = motion.tween({
     duration: 30000,
     ease: motion.easing.easeInOut,
     values: {
-        scale: 500
+        scale: 800
     }
 });
 //Simple ID gen appends an inc count on every call
-function generateID(){
-    count += 1;
-    return 'circle'+count;
+function generateID() {
+    circleCount += 1;
+    return 'circle' + circleCount;
 }
-function clean(svgID){
-    $.map(allCircles, function(val,i){
-        if(val.attr('id')==svgID){
+//Find and bind opacity decreasing destruction to SVG circles
+function clean(svgID) {
+    $.map(allCircles, function (val, i) {
+        if (val.attr('id') == svgID) {
             //Fade out and destroy
-            val.animate({opacity:0},10000,mina.easeInOut,function(){
-                $('#'+val.attr('id'))[0].remove();
+            val.animate({
+                opacity: 0
+            }, 10000, mina.easeInOut, function () {
+                //Remove SVG element from DOM
+                $('#' + val.attr('id'))[0].remove();
+                //Remove SVG element from array
+                allCircles.splice(i,1);
             });
         }
     });
 }
-function animateCircle(svgID){
+
+function animateCircle(svgID) {
     //Set destruction & bind animation
     grow.set({
-        onComplete: function(state){
+        onComplete: function (state) {
             clean(svgID);
         }
-    }).on($('#'+svgID)[0]).start();
+    }).on($('#' + svgID)[0]).start();
 }
-function storeCircle(obj){
+
+function storeCircle(obj) {
     allCircles.push(obj);
 }
-function removeCircle(obj){
+
+function removeCircle(obj) {
     obj.remove();
 }
-function randomColor(){
-  let choice = Math.floor((Math.random() * 3)+1);
-  switch(choice){
-    case 1:
-      return '#DBDEE0';
-    case 2:
-      return '#BCD0DB';
-    case 3:
-      return '#FFED93';
-  }
+
+//Array Shuffler
+function shuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
-function generateRandomCircles(count = 3){
-    for(let i=0;i<count;i++){
+//Random color sequence
+function sequentialRandomColor(interval=7) {
+    switch (true) {
+    case (colorCount <= interval):
+        increment();
+        return colorArray[0];
+    case (colorCount <= interval*2):
+        increment();
+        return colorArray[1];
+    case (colorCount <= interval*3):
+        increment();
+        return colorArray[2];
+    default:
+        increment();
+        break;
+    }
+    function increment() {
+        if (colorCount < interval*3) {
+            colorCount += 1;
+        } else {
+            colorCount = 0;
+        }
+    }
+}
+
+function generateRandomCircles(count = 3) {
+    for (let i = 0; i < count; i++) {
         var screenHeight = $(window).height();
         var screenWidth = $(window).width();
-        console.log(screenWidth);
-        let x = Math.floor(Math.random() * ((screenWidth-0)+1) + (1));
-        let y = Math.floor((Math.random() * screenHeight) + 1)+1;
+        let x = Math.floor(Math.random() * ((screenWidth - 0) + 1) + (1));
+        let y = Math.floor((Math.random() * screenHeight) + 1) + 1;
         let unique = generateID();
         storeCircle(
-            (s.circle(x,y,1)).attr({
-                fill: randomColor(),
+            (s.circle(x, y, 1)).attr({
+                fill: sequentialRandomColor(6),
                 id: unique
             })
         );
         animateCircle(unique);
     }
 }
-var begin = setInterval(()=>generateRandomCircles(1),1000);
+var begin = setInterval(() => generateRandomCircles(1), 3000);
 //window.onblur(window.clearInterval(begin));
 
 //Old animation need to reconfigure
