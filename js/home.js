e2fd5613861d69d28845b415559a2b9b38077a71
Yash Gopal <reach.yashg@gmail.com>
2016-08-25 15:45:10 -0400

    Fixed smoothState by updating jQuery dependency. Set default structure of pages

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 //Keep track of whether the browser tab or window is in focus. If not, pause animations and other background js processes for performance
-$(window).focus(function() {
-	window_focus = true;
-}).blur(function() {
-	window_focus = false;
+$(window).focus(function () {
+    window_focus = true;
+}).blur(function () {
+    window_focus = false;
 });
 /*$('body').flowtype({
  minimum   : 0,
@@ -12,57 +12,57 @@ $(window).focus(function() {
  fontRatio : 40
 });*/
 //Toggle current for clicked menu links
-$(function (){
-	[].slice.call(document.querySelectorAll('.menu')).forEach(function(menu) {
-		var menuItems = menu.querySelectorAll('.menu__link'),
-		setCurrent = function(ev) {
-			ev.preventDefault();
+/*$(function (){
+    [].slice.call(document.querySelectorAll('.menu')).forEach(function(menu) {
+        var menuItems = menu.querySelectorAll('.menu__link'),
+        setCurrent = function(ev) {
+            ev.preventDefault();
 
-						var item = ev.target.parentNode; // li
+                        var item = ev.target.parentNode; // li
 
-						// return if already current
-						if ($(item).hasClass('menu__item--current')) {
-							return false;
-						}
-						// remove current
-						$('.menu__item--current').removeClass('menu__item--current');
-						// set current
-						$(item).addClass('menu__item--current')
-					};
+                        // return if already current
+                        if ($(item).hasClass('menu__item--current')) {
+                            return false;
+                        }
+                        // remove current
+                        $('.menu__item--current').removeClass('menu__item--current');
+                        // set current
+                        $(item).addClass('menu__item--current')
+                    };
 
-					[].slice.call(menuItems).forEach(function(el) {
-						el.addEventListener('click', setCurrent);
-					});
-				});
+                    [].slice.call(menuItems).forEach(function(el) {
+                        el.addEventListener('click', setCurrent);
+                    });
+                });
 });
 $("#work").on('click',function(){
-	$(".page").load("work.html");
-});
-/*$(function(){
-	'use strict';
-	var $page = $('#main'),
-	options = {
-		debug: true,
-		prefetch: true,
-		cacheLength: 4,
-		onStart: {
-          duration: 350, // Duration of our animation
-          render: function ($container) {
-            // Add your CSS animation reversing class
-            $container.addClass('is-exiting');
-            // Restart your animation
-            smoothState.restartCSSAnimations();
-        }
-    },
-    onReady: {
-    	duration: 0,
-    	render: function ($container, $newContent) {
-            // Remove your CSS animation reversing class
-            $container.removeClass('is-exiting');
-            // Inject the new content
-            $container.html($newContent);
-        }
-    }
-},
-smoothState = $page.smoothState(options).data('smoothState');
+    $(".page").load("work.html");
 });*/
+$(function () {
+    'use strict';
+    var $page = $('#main'),
+        options = {
+            debug: true,
+            prefetch: true,
+            cacheLength: 5,
+            onStart: {
+                duration: 350, // Duration of our animation
+                render: function ($container) {
+                    // Add your CSS animation reversing class
+                    $container.addClass('is-exiting');
+                    // Restart your animation
+                    smoothState.restartCSSAnimations();
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    // Remove your CSS animation reversing class
+                    $container.removeClass('is-exiting');
+                    // Inject the new content
+                    $container.html($newContent);
+                }
+            }
+        },
+        smoothState = $page.smoothState(options).data('smoothState');
+});
